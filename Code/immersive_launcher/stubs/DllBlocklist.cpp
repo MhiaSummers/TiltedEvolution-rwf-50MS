b5bda85b5aebd1762e17c34b95b166b1fe4e6759
Richard Fortier <rwf@ourchateau.net>
2025-05-16 20:13:33 -0400

    Unblock EngineFixes for testing.

--- Code/immersive_launcher/stubs/DllBlocklist.cpp
+++ Code/immersive_launcher/stubs/DllBlocklist.cpp
@@ -8,7 +8,7 @@ namespace stubs
     // A list of modules blocked for a variety of reasons, but mainly
     // for causing crashes and incompatibility with ST
     const wchar_t* const kDllBlocklist[] = {
-        L"EngineFixes.dll",          // Skyrim Engine Fixes, breaks our hooks
+ //     L"EngineFixes.dll",          // Skyrim Engine Fixes, breaks our hooks
         L"SkyrimSoulsRE.dll",        // Our mod implements this with special handling
         L"crashhandler64.dll",       // Stream crash handler, breaks heap
         L"fraps64.dll",              // Breaks tilted ui
